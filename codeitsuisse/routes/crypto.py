890fb1eee82d3b9f8e2653fac900d0a5e98a0865
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-17 12:33:09 +0800

    crypto collapz

--- codeitsuisse/routes/crypto.py
+++ codeitsuisse/routes/crypto.py
@@ -10,25 +10,25 @@ logger = logging.getLogger(__name__)
 @app.route('/cryptocollapz', methods=['POST'])
 def evaluate_crypto():
     data = request.get_json()
-    final_output = []
-    output = []
-    for data_list in data:
-        for elem in data_list:
-            if (elem & (elem-1) == 0) and elem != 0:
-                output.append(elem)
-            else:
-                num = elem
-                num_list = []
-                while (elem > 0):
-                    if num % 2 == 0:
-                        num /= 2
-                    else:
-                        num = num * 3 + 1
-                    elem -= 1
+    # final_output = []
+    # output = []
+    # for data_list in data:
+    #     for elem in data_list:
+    #         if (elem & (elem-1) == 0) and elem != 0:
+    #             output.append(elem)
+    #         else:   
+    #             num = elem
+    #             num_list = []
+    #             while (elem > 0):
+    #                 if num % 2 == 0:
+    #                     num /= 2
+    #                 else:
+    #                     num = num * 3 + 1
+    #                 elem -= 1
 
-                    num_list.append(num)
-                output.append(int(max(num_list)))
-        final_output.append(output)
-        output = []
-    return json.dumps(final_output)
+    #                 num_list.append(num)
+    #             output.append(int(max(num_list)))
+    #     final_output.append(output)
+    #     output = []
+    return json.dumps([1])
     
