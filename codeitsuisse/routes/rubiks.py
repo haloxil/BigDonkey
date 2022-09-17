d955bf344ea1818d12cf7732fdfc5519d1b72548
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-18 02:25:42 +0800

    final bless 300

---
+++ codeitsuisse/routes/rubiks.py
@@ -0,0 +1,25 @@
+from curses.ascii import isspace
+import logging
+import json
+import numpy as np
+
+from flask import request, Response
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/rubiks', methods=['POST'])
+def evaluate_rubiks():
+    data = request.get_json()
+    ops = data["ops"]
+    state = data["state"]
+
+    op = "U"
+    if op == "U":
+        state['u'] = (np.array(state['u']).transpose).tolist()
+
+    print(ops)
+    print(state)
+    return json.dumps("hello")
