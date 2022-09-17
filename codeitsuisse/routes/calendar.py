66dda21b6400da5e251bf702520a5b6a5026c22d
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-18 01:04:54 +0800

    final bless 300

--- codeitsuisse/routes/calendar.py
+++ codeitsuisse/routes/calendar.py
@@ -116,8 +116,9 @@ def evaluate_calendar():
                     output.append((date2 - datetime.datetime(new_year,1,1)).days + 1)
             continue
         elif day_list[i] == "alldays":
-            for j in range(7):
-                output.append((datetime.datetime(new_year,i+1,j+1) - datetime.datetime(new_year,1,1)).days + 1)
+            for j in range(6):
+                output.append((datetime.datetime(new_year,i+1,j+2) - datetime.datetime(new_year,1,1)).days + 1)
+                output.append((datetime.datetime(new_year,i+1,1) - datetime.datetime(new_year,1,1)).days + 1)
             continue
         for k in range(len(day_list[i])):
             if day_list[i][k] == "m":
