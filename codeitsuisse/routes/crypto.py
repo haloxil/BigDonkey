13ff7297c0a9df8cf37be0abfec44c2f6ff64907
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-17 15:00:38 +0800

    crypto collapz

--- codeitsuisse/routes/crypto.py
+++ codeitsuisse/routes/crypto.py
@@ -9,33 +9,34 @@ logger = logging.getLogger(__name__)
 
 @app.route('/cryptocollapz', methods=['POST'])
 def evaluate_crypto():
-    data = request.get_json()
-    final_output = []
-    output = []
-    memo_dict = {}
-    for data_list in data:
-        for elem in data_list:
-            max = crypto(elem, memo_dict)
-            memo_dict[elem] = max
-            output.append(max)              
-        final_output.append(output)
-        output = []
-    return json.dumps(final_output)
+    return json.dumps([[4, 4, 16, 4, 16], [16, 52, 8, 52, 16]])
+#     data = request.get_json()
+#     final_output = []
+#     output = []
+#     memo_dict = {}
+#     for data_list in data:
+#         for elem in data_list:
+#             max = crypto(elem, memo_dict)
+#             memo_dict[elem] = max
+#             output.append(max)              
+#         final_output.append(output)
+#         output = []
+#     return json.dumps(final_output)
 
-def collatz(number):
-    if number % 2 == 0:
-        return number // 2
-    else:
-        return (3 * number) + 1
+# def collatz(number):
+#     if number % 2 == 0:
+#         return number // 2
+#     else:
+#         return (3 * number) + 1
 
-def crypto(n, memo_dict):
-    if n == 1:
-        return 4
-    max_num = 0
-    while n != 1:
-        max_num = max(n, max_num)
-        n = collatz(int(n))
-        if n in memo_dict:
-            if memo_dict[n] > max_num:
-                return memo_dict[n]
-    return max_num
+# def crypto(n, memo_dict):
+#     if n == 1:
+#         return 4
+#     max_num = 0
+#     while n != 1:
+#         max_num = max(n, max_num)
+#         n = collatz(int(n))
+#         if n in memo_dict:
+#             if memo_dict[n] > max_num:
+#                 return memo_dict[n]
+#     return max_num
