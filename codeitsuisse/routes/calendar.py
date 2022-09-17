a81c34d295abb2025e507ef72f7705d9c212e41f
Sean Evan Seah <seanevanseah@gmail.com>
2022-09-17 23:13:35 +0800

    bless 150

--- codeitsuisse/routes/calendar.py
+++ codeitsuisse/routes/calendar.py
@@ -13,7 +13,8 @@ logger = logging.getLogger(__name__)
 @app.route('/calendarDays', methods=['POST'])
 def evaluate_calendar():
     data = request.get_json()
-    count = 1
+    count = 0
+    index = 0
     part1 = ""
     date_dic = {}
 
@@ -23,12 +24,13 @@ def evaluate_calendar():
     for day in numbers[1:]:
         date = datetime.datetime(year,1,1) + datetime.timedelta(day - 1)
         datem = date.strptime(str(date), "%Y-%m-%d %H:%M:%S")
+        month = datem.month - 1
         day_of_week = date.weekday()
-        if datem.month not in date_dic:
-            date_dic[datem.month] = str(day_of_week)
+        if month not in date_dic:
+            date_dic[month] = str(day_of_week)
         else:
-            if str(day_of_week) not in date_dic[datem.month]:
-                date_dic[datem.month] += str(day_of_week)
+            if str(day_of_week) not in date_dic[month]:
+                date_dic[month] += str(day_of_week)
 
     if not bool (date_dic):
         for i in range(12):
@@ -37,61 +39,64 @@ def evaluate_calendar():
     date_list = list(date_dic.keys())
     date_list.sort()
 
+    print(date_list)
     temp = ""
-    for key in date_list:
-        while count != 13:
-            if key != count:
-                part1 += 7 * " " + ","
-            # elif "0" in date_dic[key] and "1" in date_dic[key] and "2" in date_dic[key] and "3" in date_dic[key] and "4" in date_dic[key] and "5" in date_dic[key] and "6" in date_dic[key]:
-            #     part1 += "alldays,"
-            # elif "0" in date_dic[key] and "1" in date_dic[key] and "2" in date_dic[key] and "3" in date_dic[key] and "4" in date_dic[key]:
-            #     part1 += "weekday,"
-            # elif "5" in date_dic[key] and "6" in date_dic[key]:
-            #     part1 += "weekend,"
+
+    while count != 12:
+        if date_list[index] != count:
+            part1 += 7 * " " + ","
+        # elif "0" in date_dic[key] and "1" in date_dic[key] and "2" in date_dic[key] and "3" in date_dic[key] and "4" in date_dic[key] and "5" in date_dic[key] and "6" in date_dic[key]:
+        #     part1 += "alldays,"
+        # elif "0" in date_dic[key] and "1" in date_dic[key] and "2" in date_dic[key] and "3" in date_dic[key] and "4" in date_dic[key]:
+        #     part1 += "weekday,"
+        # elif "5" in date_dic[key] and "6" in date_dic[key]:
+        #     part1 += "weekend,"
+        else:
+            if "0" in date_dic[date_list[index]]:
+                temp += "m"
             else:
-                print(date_dic[key])
-                if "0" in date_dic[key]:
-                    temp += "m"
-                else:
-                    temp += " "
-                if "1" in date_dic[key]:
-                    temp += "t"
-                else:
-                    temp += " "
-                if "2" in date_dic[key]:
-                    temp += "w"
-                else:
-                    temp += " "
-                if "3" in date_dic[key]:
-                    temp += "t"
-                else:
-                    temp += " "
-                if "4" in date_dic[key]:
-                    temp += "f"
-                else:
-                    temp += " "
-                if "5" in date_dic[key]:
-                    temp += "s"
-                else:
-                    temp += " "
-                if "6" in date_dic[key]:
-                    temp += "s"
-                else:
-                    temp += " "
+                temp += " "
+            if "1" in date_dic[date_list[index]]:
+                temp += "t"
+            else:
+                temp += " "
+            if "2" in date_dic[date_list[index]]:
+                temp += "w"
+            else:
+                temp += " "
+            if "3" in date_dic[date_list[index]]:
+                temp += "t"
+            else:
+                temp += " "
+            if "4" in date_dic[date_list[index]]:
+                temp += "f"
+            else:
+                temp += " "
+            if "5" in date_dic[date_list[index]]:
+                temp += "s"
+            else:
+                temp += " "
+            if "6" in date_dic[date_list[index]]:
+                temp += "s"
+            else:
+                temp += " "
 
-                if temp == "mtwtfss":
-                    part1 += "alldays"
-                elif temp == "mtwtf  ":
-                    part1 += "weekday"
-                elif temp == "     ss":
-                    part1 += "weekend"
-                else:
-                    part1 += temp
-                
-                temp = ""
-                part1 += ","
+            if temp == "mtwtfss":
+                part1 += "alldays"
+            elif temp == "mtwtf  ":
+                part1 += "weekday"
+            elif temp == "     ss":
+                part1 += "weekend"
+            else:
+                part1 += temp
+            
+            temp = ""
+            part1 += ","
 
-            count += 1
+            if index < len(date_list) - 1:
+                index += 1
+
+        count += 1
 
     output_dict = {"part1": part1, "part2": []}
     return json.dumps(output_dict)
